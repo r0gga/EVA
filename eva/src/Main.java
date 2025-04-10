ff30772447708bcb64c00e349f64c8e4e8fb1258
Oskar Oetken <oskaroetken@web.de>
2025-04-10 13:32:38 +0200

    add comments to main class

--- eva/src/Main.java
+++ eva/src/Main.java
@@ -1,7 +1,8 @@
-
-
+//main class executes code
 public class Main {
+    //method that automatically runs without getting called
     public static void main(String[] args){
+        //prints "Hello World!"
         System.out.println("Hello World!");
     }
 }
